8fba03aa66a3a1ea1bceb01120a6bbff0ed7981a
diegoparradoc96 <diegoparrado.cardozo@hotmail.com>
2024-03-10 22:12:11 -0500

    ...

--- App.tsx
+++ App.tsx
@@ -3,15 +3,14 @@ import {StatusBar} from 'react-native';
 
 /* papper */
 import {MD3LightTheme as DefaultTheme, PaperProvider} from 'react-native-paper';
-
 /* context provider */
 import GlobalProvider from './src/context/GlobalProvider';
-
 /* components */
 import {DecisionAlert, InfoAlert} from './src/components';
-
 /* navigation */
 import {Navigation} from './src/navigation/Navigation';
+/* react query */
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
 /* papper theme */
 const theme = {
@@ -44,4 +43,3 @@ paleta
 lider: #303134
 monocromaticos:  #4A5A88 #43495E #445FB3 #2F5BDD
 */
-
